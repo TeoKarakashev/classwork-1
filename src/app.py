f17f9dea7b81dbd5c081dd87f72fdf306fb6b55a
Teodor Karakashev <teodor.karakashev@gmail.com>
2023-11-03 23:52:39 +0200

    msg

--- src/app.py
+++ src/app.py
@@ -10,4 +10,5 @@ def hello_world():
 if __name__ == '__main__':
     # Note the extra host argument. If we didn't have it, our Flask app
     # would only respond to requests from inside our container
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
+    
